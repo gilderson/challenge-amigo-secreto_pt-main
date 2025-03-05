5d03e96278769ec6bc00019de896247ee5004f93
gilderson <gilderson.borges@outlook.com.br>
2025-03-04 21:45:51 -0300

    Criando lista de amigos e mostrando no HTML

--- app.js
+++ app.js
@@ -2,30 +2,38 @@ let lista = [];//array para agrupar os nomes dos amigos
 
 let amigos = document.getElementById('listaAmigos');
 
-let ul = document.createElement('ul');
+
 
 function adicionarAmigo() {
     let nomes = document.querySelector('input').value
-     
-    if (nomes == ''){
+
+    if (nomes == '') {
         alert('O campo não pode ficar em branco, informe um nome');
     } else {
-       lista.push(nomes);
-       console.log(lista);
-       console.log(lista[1]);
-       limparCampo();
+        lista.push(nomes);
+        console.log(lista);
+        console.log(lista[1]);
+        limparCampo();
+        listaAmigos();
     }
-    for (let i = 0; i < lista.length; i++){
-        let li = document.createElement('li');
-        li.textContent = lista[i];
-        ul.appendChild(li);
-    }
-    amigos.appendChild(ul);
 };
 
 function limparCampo() {
     nomes = document.querySelector('input')
-    nomes.value="";
+    nomes.value = "";
 }
 
 adicionarAmigo();
+
+function listaAmigos() {
+
+    let listaHTML = "<ul>";
+
+    for (let i = 0; i < lista.length; i++) {
+        listaHTML += "<li>" + lista[i] + "</li>";
+    }
+
+    listaHTML += "</ul>"
+
+    amigos.innerHTML = listaHTML;
+}
