0e04e69cb8970761f8d7db2fc7981bfa9cca306b
gilderson <gilderson.borges@outlook.com.br>
2025-03-05 23:25:37 -0300

    Amigo sorteado

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ let listaDeAmigosSorteio = [];//array para agrupar os nomes dos amigos
 
 let amigos = document.getElementById('listaAmigos');
 
+let resultadoSorteio = document.getElementById('resultado');
+
+//função para adicionar amigos na lista que será gerada
 function adicionarAmigo() {
     let nomes = document.querySelector('input').value
 
@@ -27,4 +30,16 @@ function adicionarAmigo() {
 function limparCampo() {
     nomes = document.querySelector('input')
     nomes.value = "";
+}
+
+function sortearAmigo() {
+    if (listaDeAmigosSorteio.length === 0){
+        alert ('Adicione amigos antes de sortear.');
+        return;
+    }
+    
+    let amigoAleatorio = Math.floor(Math.random() * listaDeAmigosSorteio.length);
+    let amigoSorteado = listaDeAmigosSorteio[amigoAleatorio];
+
+    resultadoSorteio.textContent = `Amigo sorteado: ${amigoSorteado}`;
 }
